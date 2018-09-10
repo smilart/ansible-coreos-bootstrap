c236a692cb5b6a28058d3c8143ca666ef27655c4
Sergei Diachenko <diachenko.s.a@gmail.com>
2018-09-10 15:24:38 +0300

    change python directory

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -1,18 +1,20 @@
 #/bin/bash
 
+PYTHON_HOME=/opt/python
+
 set -e
 
 cd
 
-if [[ -e $HOME/.bootstrapped ]]; then
+if [[ -e $PYTHON_HOME/.bootstrapped ]]; then
   exit 0
 fi
 
 PYPY_VERSION=6.0.0
 
-if [[ -e $HOME/pypy-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 ]]; then
-  tar -xjf $HOME/pypy-$PYPY_VERSION-linux_x86_64-portable.tar.bz2
-  rm -rf $HOME/pypy-$PYPY_VERSION-linux_x86_64-portable.tar.bz2
+if [[ -e $PYTHON_HOME/pypy-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 ]]; then
+  tar -xjf $PYTHON_HOME/pypy-$PYPY_VERSION-linux_x86_64-portable.tar.bz2
+  rm -rf $PYTHON_HOME/pypy-$PYPY_VERSION-linux_x86_64-portable.tar.bz2
 else
   wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 |tar -xjf -
 fi
@@ -21,17 +23,17 @@ mv -n pypy-$PYPY_VERSION-linux_x86_64-portable pypy
 
 ## library fixup
 mkdir -p pypy/lib
-[ -f /lib64/libncurses.so.5.9 ] && ln -snf /lib64/libncurses.so.5.9 $HOME/pypy/lib/libtinfo.so.5
-[ -f /lib64/libncurses.so.6.1 ] && ln -snf /lib64/libncurses.so.6.1 $HOME/pypy/lib/libtinfo.so.5
+[ -f /lib64/libncurses.so.5.9 ] && ln -snf /lib64/libncurses.so.5.9 $PYTHON_HOME/pypy/lib/libtinfo.so.5
+[ -f /lib64/libncurses.so.6.1 ] && ln -snf /lib64/libncurses.so.6.1 $PYTHON_HOME/pypy/lib/libtinfo.so.5
 
-mkdir -p $HOME/bin
+mkdir -p $PYTHON_HOME/bin
 
-cat > $HOME/bin/python <<EOF
+cat > $PYTHON_HOME/bin/python <<EOF
 #!/bin/bash
 LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH exec $HOME/pypy/bin/pypy "\$@"
 EOF
 
-chmod +x $HOME/bin/python
-$HOME/bin/python --version
+chmod +x $PYTHON_HOME/bin/python
+$PYTHON_HOME/bin/python --version
 
-touch $HOME/.bootstrapped
+touch $PYTHON_HOME/.bootstrapped
